d366d4558b324fd6741c6968a0ee918b2de425a8
Tuan Anh <anhlt@falcongames.com>
2023-10-16 16:19:19 +0700

    update config

--- src/models/face_module.py
+++ src/models/face_module.py
@@ -3,20 +3,18 @@ from torchmetrics import MinMetric, MeanMetric
 from albumentations.pytorch.transforms import ToTensorV2
 import torch
 from lightning import LightningModule
-from torchvision.transforms import transforms
-from torchvision.models import mobilenet_v2
 from PIL import Image
 import albumentations as A
 import numpy as np
 
 
 class FaceModule(LightningModule):
-    def __init__(self) -> None:
+    def __init__(self, n_classes: int = 136, lr: float = 1e-4) -> None:
         super().__init__()
+        self.save_hyperparameters(logger=False)
         self.net = torch.hub.load('pytorch/vision:v0.9.0', 'mobilenet_v2', pretrained=True)
-        self.net.classifier = torch.nn.Linear(1280, 136)
+        self.net.classifier = torch.nn.Linear(1280, self.hparams.n_classes)
         self.criterion = torch.nn.MSELoss()
-        self.metric = MeanAbsoluteError()
 
          # for averaging loss across batches
         self.train_loss = MeanMetric()
@@ -35,33 +33,30 @@ class FaceModule(LightningModule):
         return self.net(x)
     
     def configure_optimizers(self) -> Any:
-        return torch.optim.Adam(self.parameters(), lr=1e-3)
+        return torch.optim.Adam(self.parameters(), lr=self.hparams.lr)
     
     def training_step(self, batch: Tuple[torch.Tensor, torch.Tensor], batch_idx: int) -> torch.Tensor:
         x, y = batch
         logits = self.forward(x)
         loss = self.criterion(logits, y)
-        # self.train_losses.append(loss.item())
         self.train_loss(loss)
-        self.log("train/loss", loss, on_step=True, on_epoch=False, prog_bar=True)
+        self.log("train_loss", loss, on_step=True, on_epoch=False, prog_bar=True)
         return loss
 
     def validation_step(self, batch: Tuple[torch.Tensor, torch.Tensor], batch_idx: int) -> torch.Tensor:
         x, y = batch
         logits = self.forward(x)
         loss = self.criterion(logits, y)
-        # self.val_losses.append(loss.item())
         self.val_loss(loss)
-        self.log("val/loss", loss, on_step=True, on_epoch=False, prog_bar=True)
+        self.log("val_loss", loss, on_step=True, on_epoch=False, prog_bar=True)
         return loss
 
     def test_step(self, batch: Tuple[torch.Tensor, torch.Tensor], batch_idx: int) -> torch.Tensor:
         x, y = batch
         logits = self.forward(x)
         loss = self.criterion(logits, y)
-        # self.test_losses.append(loss.item())
         self.test_loss(loss)
-        self.log("test/loss", loss, on_step=True, on_epoch=False, prog_bar=True)
+        self.log("test_loss", loss, on_step=True, on_epoch=False, prog_bar=True)
         return loss
     
     def predict_step(self, img: Image) -> Any:
@@ -74,6 +69,11 @@ class FaceModule(LightningModule):
         image = transformed['image']
         logits = self.forward(image.unsqueeze(0))
         return logits.squeeze(0)
+    
+    def on_validation_epoch_end(self) -> None:
+        acc = self.val_loss.compute()  # get current val loss
+        self.val_loss_best(acc)  # update best so far val loss
+        self.log("val_loss_best", self.val_loss_best.compute(), sync_dist=True, prog_bar=True)
 
     def save_to_state_dict(self, path: str):
         torch.save(self.state_dict(), path)
@@ -83,4 +83,4 @@ class FaceModule(LightningModule):
      
 
 if __name__ == "__main__":
-    _ = FaceModule(None, None, None, None)
+    _ = FaceModule(None)
