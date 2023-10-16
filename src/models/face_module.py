bcb51071b2f1ff64361473182e03da3f57dabbe1
Tuan Anh <mravocado102@gmail.com>
2023-10-16 09:46:49 +0700

    fave

--- src/models/face_module.py
+++ src/models/face_module.py
@@ -1,176 +1,86 @@
-from typing import Any, Dict, Tuple
-
+from typing import Any, Tuple
+from torchmetrics import MinMetric, MeanMetric
+from albumentations.pytorch.transforms import ToTensorV2
 import torch
 from lightning import LightningModule
-from torchmetrics import MeanSquaredError
+from torchvision.transforms import transforms
 from torchvision.models import mobilenet_v2
+from PIL import Image
+import albumentations as A
+import numpy as np
 
 
 class FaceModule(LightningModule):
-    def __init__(
-        self,
-        optimizer: torch.optim.Optimizer,
-        scheduler: torch.optim.lr_scheduler,
-        compile: bool,
-        n_classes: int,
-    ) -> None:
-        """Initialize a `FaceModule`.
-
-        :param net: The model to train.
-        :param optimizer: The optimizer to use for training.
-        :param scheduler: The learning rate scheduler to use for training.
-        """
+    def __init__(self) -> None:
         super().__init__()
-
-        # this line allows to access init params with 'self.hparams' attribute
-        # also ensures init params will be stored in ckpt
-        self.save_hyperparameters(logger=False)
-
-        self.net = mobilenet_v2(pretrained=True)
-        self.net.classifier = torch.nn.Linear(1280, n_classes)
-
-        # loss function
+        self.net = torch.hub.load('pytorch/vision:v0.9.0', 'mobilenet_v2', pretrained=True)
+        self.net.classifier = torch.nn.Linear(1280, 136)
         self.criterion = torch.nn.MSELoss()
+        self.metric = MeanAbsoluteError()
 
-        # self.train_loss = MeanSquaredError()
-        # self.val_loss = MeanSquaredError()
-        # self.test_loss = MeanSquaredError()
-        self.train_losses = []
-        self.val_losses = []
-        self.test_losses = []
+         # for averaging loss across batches
+        self.train_loss = MeanMetric()
+        self.val_loss = MeanMetric()
+        self.test_loss = MeanMetric()
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        """Perform a forward pass through the model `self.net`.
-
-        :param x: A tensor of images.
-        :return: A tensor of logits.
-        """
-        return self.net(x)
+        # for tracking best so far validation accuracy
+        self.val_loss_best = MinMetric()
 
     def on_train_start(self) -> None:
-        """Lightning hook that is called when training begins."""
-        # by default lightning executes validation step sanity checks before training starts,
-        # so it's worth to make sure validation metrics don't store results from these checks
-        # self.val_loss.reset()
-        # self.val_acc.reset()
-        # self.val_acc_best.reset()
-        # self.train_losses = []
-        # self.val_losses = []
-        # self.test_losses = []
-        pass
+        self.train_loss.reset()
+        self.val_loss.reset()
+        self.val_loss_best.reset()
 
-    def model_step(
-        self, batch: Tuple[torch.Tensor, torch.Tensor]
-    ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
-        """Perform a single model step on a batch of data.
-
-        :param batch: A batch of data (a tuple) containing the input tensor of images and target labels.
-
-        :return: A tuple containing (in order):
-            - A tensor of losses.
-            - A tensor of predictions.
-            - A tensor of target labels.
-        """
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.net(x)
+    
+    def configure_optimizers(self) -> Any:
+        return torch.optim.Adam(self.parameters(), lr=1e-3)
+    
+    def training_step(self, batch: Tuple[torch.Tensor, torch.Tensor], batch_idx: int) -> torch.Tensor:
         x, y = batch
         logits = self.forward(x)
         loss = self.criterion(logits, y)
-        # preds = torch.argmax(logits, dim=1)
-        return loss.item(), logits, y
+        # self.train_losses.append(loss.item())
+        self.train_loss(loss)
+        self.log("train/loss", loss, on_step=True, on_epoch=False, prog_bar=True)
+        return loss
 
-    def training_step(
-        self, batch: Tuple[torch.Tensor, torch.Tensor], batch_idx: int
-    ):
-        """Perform a single training step on a batch of data from the training set.
+    def validation_step(self, batch: Tuple[torch.Tensor, torch.Tensor], batch_idx: int) -> torch.Tensor:
+        x, y = batch
+        logits = self.forward(x)
+        loss = self.criterion(logits, y)
+        # self.val_losses.append(loss.item())
+        self.val_loss(loss)
+        self.log("val/loss", loss, on_step=True, on_epoch=False, prog_bar=True)
+        return loss
 
-        :param batch: A batch of data (a tuple) containing the input tensor of images and target
-            labels.
-        :param batch_idx: The index of the current batch.
-        :return: A tensor of losses between model predictions and targets.
-        """
-        self.net.train()
-        loss, preds, targets = self.model_step(batch)
+    def test_step(self, batch: Tuple[torch.Tensor, torch.Tensor], batch_idx: int) -> torch.Tensor:
+        x, y = batch
+        logits = self.forward(x)
+        loss = self.criterion(logits, y)
+        # self.test_losses.append(loss.item())
+        self.test_loss(loss)
+        self.log("test/loss", loss, on_step=True, on_epoch=False, prog_bar=True)
+        return loss
+    
+    def predict_step(self, img: Image) -> Any:
+        t = A.Compose([
+            A.Resize(244, 244),
+            A.Normalize(),
+            ToTensorV2(),
+        ])
+        transformed = t(image=np.array(img))
+        image = transformed['image']
+        logits = self.forward(image.unsqueeze(0))
+        return logits.squeeze(0)
 
-        # update and log metrics
-        self.train_losses.append(loss)
-        self.log("train/loss", loss, on_step=False, on_epoch=True, prog_bar=True)
+    def save_to_state_dict(self, path: str):
+        torch.save(self.state_dict(), path)
 
-    def on_train_epoch_end(self) -> None:
-        "Lightning hook that is called when a training epoch ends."
-        pass
-
-    def validation_step(self, batch: Tuple[torch.Tensor, torch.Tensor], batch_idx: int) -> None:
-        """Perform a single validation step on a batch of data from the validation set.
-
-        :param batch: A batch of data (a tuple) containing the input tensor of images and target
-            labels.
-        :param batch_idx: The index of the current batch.
-        """
-        self.net.eval()
-        loss, preds, targets = self.model_step(batch)
-
-        # update and log metrics
-        self.val_losses.append(loss)
-        self.log("val/loss", loss, on_step=False, on_epoch=True, prog_bar=True)
-
-    def on_validation_epoch_end(self) -> None:
-        "Lightning hook that is called when a validation epoch ends."
-        # log `val_acc_best` as a value through `.compute()` method, instead of as a metric object
-        # otherwise metric would be reset by lightning after each epoch
-        pass
-
-    def test_step(self, batch: Tuple[torch.Tensor, torch.Tensor], batch_idx: int) -> None:
-        """Perform a single test step on a batch of data from the test set.
-
-        :param batch: A batch of data (a tuple) containing the input tensor of images and target
-            labels.
-        :param batch_idx: The index of the current batch.
-        """
-        self.net.eval()
-        loss, preds, targets = self.model_step(batch)
-
-        # update and log metrics
-        self.test_losses.append(loss)
-        self.log("test/loss", loss, on_step=False, on_epoch=True, prog_bar=True)
-
-    def on_test_epoch_end(self) -> None:
-        """Lightning hook that is called when a test epoch ends."""
-        pass
-
-    def setup(self, stage: str) -> None:
-        """Lightning hook that is called at the beginning of fit (train + validate), validate,
-        test, or predict.
-
-        This is a good hook when you need to build models dynamically or adjust something about
-        them. This hook is called on every process when using DDP.
-
-        :param stage: Either `"fit"`, `"validate"`, `"test"`, or `"predict"`.
-        """
-        if self.hparams.compile and stage == "fit":
-            self.net = torch.compile(self.net)
-
-    def configure_optimizers(self) -> Dict[str, Any]:
-        """Choose what optimizers and learning-rate schedulers to use in your optimization.
-        Normally you'd need one. But in the case of GANs or similar you might have multiple.
-
-        Examples:
-            https://lightning.ai/docs/pytorch/latest/common/lightning_module.html#configure-optimizers
-
-        :return: A dict containing the configured optimizers and learning-rate schedulers to be used for training.
-        """
-        optimizer = self.hparams.optimizer(params=self.parameters())
-        if self.hparams.scheduler is not None:
-            scheduler = self.hparams.scheduler(optimizer=optimizer)
-            return {
-                "optimizer": optimizer,
-                "lr_scheduler": {
-                    "scheduler": scheduler,
-                    "monitor": "val/loss",
-                    "interval": "epoch",
-                    "frequency": 1,
-                },
-            }
-        return {"optimizer": optimizer}
-
+    def load_from_state_dict(self, path: str):
+        self.load_state_dict(torch.load(path))
+     
 
 if __name__ == "__main__":
     _ = FaceModule(None, None, None, None)
